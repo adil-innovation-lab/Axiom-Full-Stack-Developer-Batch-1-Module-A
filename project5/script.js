162ef05a067d70ae7e5d6c613cec26aaf4e3ae06
adil-innovation-lab <khanadilaltaf@gmail.com>
2021-05-30 01:30:50 -0700

    Completed Project 5

--- project5/script.js
+++ project5/script.js
@@ -39,6 +39,58 @@ function addData(newUser) {
     updateDOM();
 }
 
+// Function to Double Money of All Users
+function doubleMoney() {
+    console.log('old user data', data);
+    // Loop through all users in the user data array
+    // For each user, return the user data
+    // Overwrite the data array with the new data array created by map
+    data = data.map(user => {
+        return { ...user, balance: user.balance * 2 }
+    });
+
+    console.log('new user data', data);
+
+    // Update the DOM using the new user data array
+    updateDOM();
+}
+
+// Function to filter only Millionaire Users
+function filterUsers() {
+    // Filter out all users whose balance is less than million
+    data = data.filter(user => user.balance >= 1000000);
+    // Update the DOM with new user data
+    updateDOM();
+}
+
+// Function to sort users by balance
+function sortByBalance() {
+    // Sort users by balance using a compare function inside sort
+    data = data.sort((a,b) => b.balance - a.balance)
+    // Update the DOM with new user data
+    updateDOM();
+}
+
+// Function to sum all users balance into total balance
+function totalBalance() {
+    // Update the DOM with new user data
+    updateDOM();
+    // Add up all balance from all users
+    // Accumulator starts at 0 and adds the current users balance for each iteration
+    const balance = data.reduce((acc, user) => (acc += user.balance), 0);
+    // Create a Div for the balance
+    const balanceElement = document.createElement('div');
+    // Set the innerHTML for new div
+    balanceElement.innerHTML = `<h3>Total Balance: ${formatNumberToDollar(balance)}</h3>`;
+    // Append Balance in main element
+    main.appendChild(balanceElement);
+}
+
+// Function to format random number as money
+function formatNumberToDollar(number) {
+    return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
 // Update the UI with data from the user data array
 function updateDOM(userData = data) {
     // Clear previous UI
@@ -50,11 +102,28 @@ function updateDOM(userData = data) {
         // Apply the user class to the new div
         userDiv.classList.add('user');
         // Add inner HTML to the user div
-        userDiv.innerHTML = `<strong>${user.name}</strong> ${user.balance}`
+        userDiv.innerHTML = `<strong>${user.name}</strong> 
+                            ${formatNumberToDollar(user.balance)}`
         // Add the new element into the DOM
         main.appendChild(userDiv);
     });
 }
+
+// Event Listeners
+// 1. Listen for click on Add User Button
+addUserBtn.addEventListener('click', getRandomUser);
+
+// 2. Listen for click on the Double Button
+doubleBtn.addEventListener('click', doubleMoney);
+
+// 3. Listen for click on Filter Button
+filterBtn.addEventListener('click', filterUsers);
+
+// 4. Listen for click on Sort Button
+sortBtn.addEventListener('click', sortByBalance);
+
+// 5. Listen for click on Sum Button
+sumBtn.addEventListener('click', totalBalance);
 
 
 // Create a Random User
