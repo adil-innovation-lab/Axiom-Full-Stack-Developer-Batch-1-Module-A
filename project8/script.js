1628b0a30b56f9defda93dce71687755fd729840
adil-innovation-lab <khanadilaltaf@gmail.com>
2021-06-13 01:28:37 -0700

    Project 8 done

--- project8/script.js
+++ project8/script.js
@@ -41,8 +41,78 @@ function searchMeal(e) {
     } else {
         alert('Please enter search keyword')
     };
+    // Remove previous selected meal info
+    selectedMeal.innerHTML = '';
+};
+
+// Function to get details of selected meal
+function getMeal(mealId) {
+    // Fetch details of meal using the mealId
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+            // Render in the UI
+            displayMealDetails(meal);
+        });
+}
+
+// Function to render meal details in UI
+function displayMealDetails(meal) {
+    // Clear Search Reults
+    meals.innerHTML = '';
+    resultsHeading.innerHTML = '';
+    // Array to hold ingredients & measurements
+    const ingredients = [];
+    // Loop over ingredient attributes
+    for ( let i = 1; i <= 20; i++ ) {
+        // Check if ingredient exists
+        if ( meal[`strIngredient${i}`] ) {
+            // Push all the ingredients and measurements into the ingredients array
+            ingredients.push(`${meal[`strIngredient${i}`]}: ${meal[`strMeasure${i}`]}`);
+        } else {
+            break;
+        }
+    };
+    // Render data into UI
+    selectedMeal.innerHTML = `
+        <div class="selected-meal-details">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt"${meal.strMeal}" />
+            <div class="selected-meal-info" >
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : '' }
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : '' }
+            </div>
+            <div class="selected-meal-instructions">
+                <p>${meal.strInstructions}</p>
+                <h3>Ingredients</h3>
+                <ul>
+                    ${ingredients.map( ingredient => `<li>${ingredient}</li>` ).join('')}
+                </ul>
+            </div>
+        </div>
+    `;
 };
 
 // Event Listeners
 // 1. Listen for form submit
 submit.addEventListener('submit', searchMeal)
+
+// 2. Listen for click on meals 
+meals.addEventListener('click', e => {
+    // Find and return only if clicked on a meal-info div
+    const mealInfo = e.path.find(item => {
+        if (item.classList) {
+            return item.classList.contains('meal-info');
+        } else {
+            return false;
+        }
+    });
+    // Check if mealInfo exists
+    if (mealInfo) {
+        // Get the data-mealid attribute
+        const mealId = mealInfo.getAttribute('data-mealid');
+        // Fetch details of meal
+        getMeal(mealId);
+    }
+});
